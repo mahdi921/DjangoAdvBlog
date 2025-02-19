2eb02892dfb09f330778ae03138e8cd8fabda509
mahdi921 <mahdi.afshar921@gmail.com>
2025-02-19 20:25:56 +0330

    add Profile model and migration for user profiles

--- core/accounts/models.py
+++ core/accounts/models.py
@@ -52,3 +52,17 @@ class User(AbstractBaseUser, PermissionsMixin):
     objects = UserManager()
     def __str__(self):
         return self.email
+
+
+class Profile(models.Model):
+    user = models.ForeignKey("User", on_delete=models.CASCADE)
+    # username= models.CharField(max_length=255, blank=True, null=True)
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    bio = models.TextField(max_length=500, blank=True, null=True)
+    avatar = models.ImageField(blank=True, null=True)
+    created_date = models.DateTimeField(auto_now_add=True)
+    updated_date = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.user.email
