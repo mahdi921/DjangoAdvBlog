25eaba55036a0a6821a55a2d6f148e92c304feeb
mahdi921 <mahdi.afshar921@gmail.com>
2025-02-20 02:00:10 +0330

    imported user creation signal module to apps.py -> ready()

--- core/accounts/apps.py
+++ core/accounts/apps.py
@@ -4,3 +4,9 @@ from django.apps import AppConfig
 class AccountsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'accounts'
+
+    def ready(self):
+        # this will import signal from accounts.signals
+        # to do profile creation after user creation
+        # or after user update
+        import accounts.signals
