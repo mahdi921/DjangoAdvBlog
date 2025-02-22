07def8a1e9669051d7c386703030aec11b315d2f
mahdi921 <mahdi.afshar921@gmail.com>
2025-02-22 15:44:22 +0330

    add function and class-based views for index page

--- core/blog/views.py
+++ core/blog/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from django.views.generic.base import TemplateView
+from blog.models import Post
+from django.utils import timezone
 
-# Create your views here.
+def index_view(request):
+    # an example of function based view
+    context = {
+        'name': 'Mahdi1'
+    }
+    return render(request, 'index.html', context)
+
+class IndexView(TemplateView):
+    # an example of class based view using TemplateView built-in class
+    template_name = 'index.html'
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        # context['posts'] = Post.objects.filter(status=1, published_date__lte=timezone.now())
+        context['name'] = 'Mahdi2'
+        return context
