a5c6c77d13cce84eee6d81f317ab0ad4c6da4aff
mahdi921 <19709896+mahdi921@users.noreply.github.com>
2025-03-15 04:01:06 +0330

    update post_list and post_detail views to include permission classes and support PUT and DELETE methods

--- core/blog/api/v1/views.py
+++ core/blog/api/v1/views.py
@@ -1,11 +1,16 @@
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, permission_classes
 from rest_framework.response import Response
+from rest_framework.permissions import (
+    IsAuthenticatedOrReadOnly  # , IsAuthenticated
+)
 from .serializers import PostSerializer
 from blog.models import Post
 from django.shortcuts import get_object_or_404
+from rest_framework import status
 
 
 @api_view(['GET', 'POST'])
+@permission_classes([IsAuthenticatedOrReadOnly])
 def post_list(request):
     if request.method == 'GET':
         posts = Post.objects.filter(status=True)
@@ -17,8 +22,19 @@ def post_list(request):
         serializer.save()
         return Response(request.data)
 
-@api_view('GET', 'PUT')
+
+@api_view(['GET', 'PUT', 'DELETE'])
 def post_detail(request, id):
     post = get_object_or_404(Post, id=id, status=True)
-    serializer = PostSerializer(post)
-    return Response(serializer.data)
+    if request.method == 'GET':
+        serializer = PostSerializer(post)
+        return Response(serializer.data)
+    elif request.method == 'PUT':
+        serializer = PostSerializer(post, data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
+    elif request.method == 'DELETE':
+        post.delete()
+        return Response({'detail': 'item removed successfully'},
+                        status=status.HTTP_204_NO_CONTENT)
