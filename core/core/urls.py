f65b2057c202629f766c3a6bdd7c7089a9375e7a
mahdi921 <19709896+mahdi921@users.noreply.github.com>
2025-03-15 04:01:26 +0330

    add API authentication URLs and format urlpatterns for clarity

--- core/core/urls.py
+++ core/core/urls.py
@@ -23,9 +23,11 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('blog/', include('blog.urls')),
     path("accounts/", include("django.contrib.auth.urls")),
+    path('api-auth/', include('rest_framework.urls')),
     # path('accounts/', include('accounts.urls')),
-] 
+]
 if settings.DEBUG:
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
+    urlpatterns += static(settings.STATIC_URL,
+                          document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
