3f4b21cbe46f0c500d7d7519d7686008ba159c9c
mahdi921 <19709896+mahdi921@users.noreply.github.com>
2025-03-23 14:52:30 +0330

    add custom action to PostModelViewSet for health check endpoint

--- core/blog/api/v1/views.py
+++ core/blog/api/v1/views.py
@@ -8,6 +8,7 @@ from rest_framework.permissions import (
 )
 from rest_framework import viewsets
 from rest_framework.response import Response
+from rest_framework.decorators import action
 from .serializers import PostSerializer, CategorySerializer
 from blog.models import Post,  Category
 
@@ -106,6 +107,10 @@ class PostModelViewSet(viewsets.ModelViewSet):
     permission_classes = [IsAuthenticatedOrReadOnly]
     serializer_class = PostSerializer
     queryset = Post.objects.filter(status=True)
+    
+    @action(methods=['get'], detail=False)
+    def get_ok(self, request):
+        return Response({'detail': 'ok'})
 
 
 class CategoryModelViewSet(viewsets.ModelViewSet):
