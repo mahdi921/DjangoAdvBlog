7f62a823367c5916672120d9e6a93900e93879ff
mahdi921 <19709896+mahdi921@users.noreply.github.com>
2025-03-31 01:11:03 +0330

    update accounts URL routing in urls.py to include accounts.urls for improved modularity

--- core/core/urls.py
+++ core/core/urls.py
@@ -38,7 +38,7 @@ schema_view = get_schema_view(
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('blog/', include('blog.urls')),
-    path("accounts/", include("django.contrib.auth.urls")),
+    path("accounts/", include("accounts.urls")),
     path('api-auth/', include('rest_framework.urls')),
     # path('accounts/', include('accounts.urls')),
     # Swagger documentation URLs
