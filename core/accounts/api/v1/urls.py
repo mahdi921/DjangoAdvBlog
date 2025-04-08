692b2e10725fa06a4250cfd4d256688e8353b0f2
mahdi921 <19709896+mahdi921@users.noreply.github.com>
2025-04-08 15:55:35 +0330

    Update urlpatterns in urls.py to include registration and token login endpoints for accounts API v1

--- core/accounts/api/v1/urls.py
+++ core/accounts/api/v1/urls.py
@@ -1,9 +1,15 @@
-# from django.urls import path
+from django.urls import path
+from rest_framework.authtoken.views import obtain_auth_token
 
-from accounts.api.v1 import views
+from . import views
 
 app_name = "api-v1"
 
 urlpatterns = [
-    
+    # registration
+    path('registration/',
+         views.RegistrationApiView.as_view(),
+         name='registration'
+         ),
+    path('token/login', obtain_auth_token, name='token-login'),
 ]
