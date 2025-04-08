bfaba691a84f9891ffb7be27001ee8581b263363
mahdi921 <19709896+mahdi921@users.noreply.github.com>
2025-04-08 17:07:49 +0330

    Add CustomDiscardAuthToken view to allow authenticated users to delete their auth tokens

--- core/accounts/api/v1/views.py
+++ core/accounts/api/v1/views.py
@@ -4,6 +4,8 @@ from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.authtoken.models import Token
+from rest_framework.views import APIView
+from rest_framework.permissions import IsAuthenticated
 
 
 class RegistrationApiView(generics.GenericAPIView):
@@ -25,6 +27,7 @@ class RegistrationApiView(generics.GenericAPIView):
 
 class CustomObtainAuthToken(ObtainAuthToken):
     serializer_class = CustomAuthTokenSerializer
+
     def post(self, request, *args, **kwargs):
         serializer = self.serializer_class(data=request.data,
                                            context={'request': request})
@@ -36,3 +39,11 @@ class CustomObtainAuthToken(ObtainAuthToken):
             'user_id': user.pk,
             'email': user.email
         })
+
+
+class CustomDiscardAuthToken(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def post(self, request):
+        request.user.auth_token.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
