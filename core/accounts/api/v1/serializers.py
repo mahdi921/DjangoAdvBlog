68c1fb1edce75de8b3d2536b0f9046a96e0d485a
mahdi921 <19709896+mahdi921@users.noreply.github.com>
2025-04-12 01:22:36 +0330

    Add CustomTokenObtainPairSerializer to include user email and ID in token validation

--- core/accounts/api/v1/serializers.py
+++ core/accounts/api/v1/serializers.py
@@ -4,6 +4,7 @@ from django.contrib.auth.password_validation import validate_password
 from django.core import exceptions
 from django.contrib.auth import authenticate
 from django.utils.translation import gettext_lazy as _
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 
 
 User = get_user_model()
@@ -74,3 +75,11 @@ class CustomAuthTokenSerializer(serializers.Serializer):
 
         attrs['user'] = user
         return attrs
+
+
+class CustomTokenObtainPairSerializer(TokenObtainPairSerializer):
+    def validate(self, attrs):
+        validated_data = super().validate(attrs)
+        validated_data['email'] = self.user.email
+        validated_data['user_id'] = self.user.id
+        return validated_data
