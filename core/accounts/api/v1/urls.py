0a17f4ae0b630ffbc111c073d25e963046c9b36e
mahdi921 <19709896+mahdi921@users.noreply.github.com>
2025-04-12 15:50:33 +0330

    Add change password url

--- core/accounts/api/v1/urls.py
+++ core/accounts/api/v1/urls.py
@@ -12,7 +12,6 @@ from . import views
 app_name = "api-v1"
 
 urlpatterns = [
-    # registration
     path('registration/',
          views.RegistrationApiView.as_view(),
          name='registration'
@@ -21,12 +20,17 @@ urlpatterns = [
         'token/login/',
         views.CustomObtainAuthToken.as_view(),
         name='token-login'
-        ),
+    ),
     path(
         'token/logout/',
         views.CustomDiscardAuthToken.as_view(),
         name='token-logout'
-        ),
+    ),
+    path(
+        'change-password/',
+        views.ChangePasswordApiView.as_view(),
+        name='change-password'
+    ),
     path('jwt/create/', TokenObtainPairView.as_view(), name='jwt-create'),
     path('jwt/refresh/', TokenRefreshView.as_view(), name='jwt-refresh'),
     path('jwt/verify/', TokenVerifyView.as_view(), name='jwt-verify'),
