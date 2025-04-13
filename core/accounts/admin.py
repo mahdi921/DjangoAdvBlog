69e03b7cd5a90949754080ddc3899395ceea87b8
mahdi921 <19709896+mahdi921@users.noreply.github.com>
2025-04-13 22:27:53 +0330

    Add is_verified field to User admin display and permissions

--- core/accounts/admin.py
+++ core/accounts/admin.py
@@ -10,8 +10,8 @@ class CustomUserAdmin(UserAdmin):
     # The fields to be used in displaying the User model.
     # These override the definitions on the base UserAdmin
     # that reference specific fields on auth.User.
-    list_display = ['email', 'is_superuser', 'is_active']
-    list_filter = ['email', 'is_superuser', 'is_active']
+    list_display = ['email', 'is_superuser', 'is_active', 'is_verified']
+    list_filter = ['email', 'is_superuser', 'is_active', 'is_verified']
     search_fields = ['email']
     ordering = ['email']
     filter_horizontal = []
@@ -22,7 +22,7 @@ class CustomUserAdmin(UserAdmin):
         ),
         ("Permissions", {
             "fields": (
-                "is_superuser", "is_staff", "is_active",
+                "is_superuser", "is_staff", "is_active", 'is_verified'
             )
         }
         ),
@@ -46,7 +46,8 @@ class CustomUserAdmin(UserAdmin):
             "classes": ("wide",),
             "fields": (
                 "email", "password1", "password2", "is_superuser",
-                "is_staff", "is_active", "groups", "user_permissions"
+                "is_staff", "is_active", 'is_verified', "groups",
+                "user_permissions"
             )}
          ),
     )
