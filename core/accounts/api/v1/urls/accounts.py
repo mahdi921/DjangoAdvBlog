9fb320a0f33674972aad855b10b0a6e96f2191e5
mahdi921 <19709896+mahdi921@users.noreply.github.com>
2025-04-20 01:23:08 +0330

    Add activation confirmation and resend endpoints to URL patterns

--- core/accounts/api/v1/urls/accounts.py
+++ core/accounts/api/v1/urls/accounts.py
@@ -23,8 +23,10 @@ urlpatterns = [
         name='token-logout'
     ),
     path('test-email/', views.TestEmailSend.as_view(), name='test-email'),
-    # path('activation/confirm/',),
-    # path('activation/resend/',),
+    path('activation/confirm/<str:token>',
+         views.ActivationApiView.as_view(), name='activation'),
+    path('activation/resend/', views.ActivationResendApiView.as_view(),
+         name='activation-resend'),
     path(
         'change-password/',
         views.ChangePasswordApiView.as_view(),
